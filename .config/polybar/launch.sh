0c188816182f10057b04adea10ec86f7c7db6110
Agustín Ávila Marín <agustin.avila@mekitec.com>
2022-03-26 11:38:36 -0600

    My important dotfiles

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+fc-cache --force
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar first 2>&1 | tee -a /tmp/polybar.log & disown
+polybar second 2>&1 | tee -a /tmp/polybar.log & disown
+
+#for m in $(polybar --list-monitors | cut -d":" -f1);
+#do
+#    MONITOR=$m polybar -r top &
+#done
+
+echo "Polybar launched..."
